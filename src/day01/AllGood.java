9ceb8be63fc454ab95399b0926f2578bf8c79cc8
Walid Saleh <wideopennew1@gmail.com>
2020-04-24 15:03:35 -0400

    first commit

---
+++ src/day01/AllGood.java
@@ -0,0 +1,9 @@
+package day01;
+
+import javax.sound.midi.Soundbank;
+
+public class AllGood {
+    public static void main(String[] args) {
+        System.out.println("I Hope Everything Is Good");
+    }
+}
